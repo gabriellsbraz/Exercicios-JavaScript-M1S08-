6a377a5d0aedf75a83d252ffd34c45fd6d8f1afa
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-08-05 10:07:55 -0300

    [M1S08] Ex 1 - [Dados] Criando um JSON com os dados

---
+++ dados.js
@@ -0,0 +1,24 @@
+const usuarios = `
+  [
+    {
+      "nome": "Admin",
+      "email": "teste@email.com",
+      "senha" : admin123
+    },
+    {
+      "nome": "Gabriel",
+      "email": "gabriel@email.com",
+      "senha" : gb1234
+    },
+    {
+      "nome": "Bruno",
+      "email": "teste@email.com",
+      "senha" : br1234
+    },
+    {
+      "nome": "Camila",
+      "email": "camila@email.com",
+      "senha" : cah1234
+    }
+  ]
+`
