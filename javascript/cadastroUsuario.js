1bcf9fb90d45b22302a588f7dc55527c831d611f
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-08-06 17:05:13 -0300

    [M1S08] Ex 7 - [Lógica] Criando função para cadastrar um novo usuário

---
+++ javascript/cadastroUsuario.js
@@ -0,0 +1,21 @@
+function cadastrarUsuario(event){
+  event.preventDefault();
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+
+  let listaUsuarios = JSON.parse(usuarios);
+
+  let [usuarioEncontrado] = listaUsuarios.filter(usuario => usuario.email === email);
+
+  if (usuarioEncontrado) {
+    alert("Email já cadastrado!");
+    return;
+  }
+
+  listaUsuarios.push({nome, email, senha});
+
+  listaUsuarios = JSON.stringify(listaUsuarios);
+
+  return alert(`Usuário(a) ${nome} cadastrado(a) com sucesso!`)
+}
